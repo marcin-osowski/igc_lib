8e1384a95668c356a2f94aa448429fc76b46f8de
Marcin Osowski <marcin@osowski.info>
2019-05-16 11:44:20 +0200

    Running tests also with python3

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e
 
+echo "Running tests with python"
 if [ -z "$1" ]; then
     # No argument provided - run all tests
     /usr/bin/env python -m unittest discover
 else
     /usr/bin/env python -m unittest $1
+fi
+
+echo "Running tests with python3"
+if [ -z "$1" ]; then
+    # No argument provided - run all tests
+    /usr/bin/env python3 -m unittest discover
+else
+    /usr/bin/env python3 -m unittest $1
 fi
