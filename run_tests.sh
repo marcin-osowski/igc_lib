7ece014ea90a65a010c20310ac3f337740413434
Marcin Osowski <marcin@osowski.info>
2019-11-27 16:20:19 +0100

    Running tests with python2 not python to get python 2.X coverage

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Running tests with python"
+echo "Running tests with python2"
 if [ -z "$1" ]; then
     # No argument provided - run all tests
-    /usr/bin/env python -m unittest discover
+    /usr/bin/env python2 -m unittest discover
 else
-    /usr/bin/env python -m unittest $1
+    /usr/bin/env python2 -m unittest $1
 fi
 
 echo "Running tests with python3"
